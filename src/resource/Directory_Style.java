9356500604b6a817da77f39845ab0b4f731a0be7
Jacob Watson <31695730+ginsax@users.noreply.github.com>
2020-07-30 20:31:45 -0400

    Initial commit

---
+++ src/resource/Directory_Style.java
@@ -0,0 +1,18 @@
+package resource;
+
+/**
+ * Static class that contains the path to resource stylesheets.
+ * @author jacob.watson
+ * @since 08/06/2019
+ */
+public class Directory_Style {
+	
+	private final static String mStylePath = "/resource/style/";
+	
+	/**
+	 * The path to the resource default stylesheet of <i>style.css</i>.
+	 * <p>
+	 * The value is equivalent to {@value #Style_Default}.
+	 */
+	public final static String Style_Default = mStylePath + "defaultStyle.css";
+}
